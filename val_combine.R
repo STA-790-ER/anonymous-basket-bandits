bb6970e8d1f27777461e4ba05aadea3c0c851b12
Joseph Lawson <jml165@dcc-login-02.oit.duke.edu>
2021-12-09 15:55:37 -0500

    make parallel result aggregation

--- val_combine.R
+++ val_combine.R
@@ -1,13 +1,12 @@
-for (t in 1:100) {
-    
-    temp = list.files(path="~/rl/valresults", pattern=paste0("*_",t,".csv"), full.names=TRUE)
-    files = lapply(temp, read.delim, header=TRUE, sep=",")
 
-    file_mats = lapply(files, as.matrix)
-    #file_array = simplify2array(file_mats)
-    stack_mat = do.call(rbind, file_mats)
-    #file_means = apply(file_array, c(1,2), mean)
-    write.csv(stack_mat, file=paste0("~/rl/valcombresults/valcombresults_",t,".csv"), row.names=TRUE)
+t=Sys.getenv("SLURM_ARRAY_TASK_ID")    
+temp = list.files(path="~/rl/valresults", pattern=paste0("*_",t,".csv"), full.names=TRUE)
+files = lapply(temp, read.delim, header=TRUE, sep=",")
 
-}
+file_mats = lapply(files, as.matrix)
+#file_array = simplify2array(file_mats)
+stack_mat = do.call(rbind, file_mats)
+#file_means = apply(file_array, c(1,2), mean)
+write.csv(stack_mat, file=paste0("~/rl/valcombresults/valcombresults_",t,".csv"), row.names=TRUE)
+
 
--- val_combine.sh
+++ val_combine.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#SBATCH -J jlcb1
+#SBATCH --array 1-100
+#SBATCH --cpus-per-task 1
+#SBATCH --output="/hpc/home/jml165/slurmout/R-%A_%a.out"
+#SBATCH --error="/hpc/home/jml165/slurmout/R-%A_%a.err"
+#SBATCH --mem=8G
+#SBATCH --partition=common,scavenger
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jml165@duke.edu
+#SBATCH --exclude=dcc-ultrasound-04,dcc-ultrasound-06,dcc-ultrasound-07
 
+module purge
 
 module load R
 
